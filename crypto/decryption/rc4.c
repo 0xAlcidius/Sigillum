0138ce4be31a2be97eee46d72c8995c245b2cb61
G00se <guus@nawijn.eu>
2025-03-22 13:37:18 +0100

    Bugfix writing files with RC4_C

--- crypto/decryption/rc4.c
+++ crypto/decryption/rc4.c
@@ -43,10 +43,31 @@ BOOL SysFunc032(IN PBYTE pRc4Key, IN PBYTE pPayload, IN DWORD dwRc4KeySize, IN D
     return TRUE;
 }
 
-int main() {
-    if (!SysFunc032(key, cipertext, sizeof(key), sizeof(cipertext))) {
-        return -1;
-    }
+DWORD WritePayload(LPWSTR filename) {
+	HANDLE hFile = CreateFile(L"image.png", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    printf("shellcode : \"%s\" \n", cipertext);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return -1;
+	}
+
+	DWORD bytesWritten;
+	if (!WriteFile(hFile, cipertext, sizeof(cipertext), &bytesWritten, NULL)) {
+		CloseHandle(hFile);
+		return -1;
+	}
+	CloseHandle(hFile);
+	return 0;
+}
+
+DWORD PrintPayload() {
+	printf("shellcode : \"%s\" \n", cipertext);
+}
+
+int main() {
+	if (!SysFunc032(key, cipertext, sizeof(key), sizeof(cipertext))) {
+		return -1;
+	}
+
+	WritePayload(L"image.png");
+	PrintPayload();
 }
